9d92181958bc419edb7dfea13efec3966de39587
Rohidas Jondhale <149058886+Rohidasjondhale@users.noreply.github.com>
2025-08-31 22:10:14 +0530

    Delete getElementsByClassName directory

--- getElementsByClassName/index.js
+++
@@ -1,8 +0,0 @@
-const fruitItems = document.getElementsByClassName("fruit");
-
-fruitItems[2].style.backgroundColor = "yellow";
-
-for (let i = 0; i < fruitItems.length; i++)
-{
-    fruitItems[i].style.fontWeight = "bold";
-}
